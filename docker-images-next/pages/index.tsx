5e25bdf8430473cd6032d570255659991f4c30e6
Ibuki Nakamura <Ibuki>
2020-07-18 23:21:05 +0900

    フロントをnext.jsに変更

---
+++ docker-images-next/pages/index.tsx
@@ -0,0 +1,14 @@
+// これがリンクAPIです
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    {/* aタグを<Link>コンポーネントで囲い、hrefなどのリンクの情報は<Link>に渡します。 */}
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default Index
