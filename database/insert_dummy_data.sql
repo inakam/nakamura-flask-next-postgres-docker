1bfad46f8effb1ef8925981e2d83088509460d5e
Ibuki Nakamura <Ibuki>
2020-07-19 18:55:53 +0900

    DBからのAPI返却を追加

--- database/insert_dummy_data.sql
+++ database/insert_dummy_data.sql
@@ -1,18 +1,19 @@
-CREATE TABLE tables (
-	user_id INT,
-    affiliate_code CHAR(100),
-    created TIMESTAMP
+CREATE TABLE users
+(
+   user_id INT,
+   affiliate_code INT,
+   created TIMESTAMP
 );
 
-INSERT INTO tables
-(
-user_id,
-affiliate_code,
-created
-)
+INSERT INTO users
+   (
+   user_id,
+   affiliate_code,
+   created
+   )
 SELECT
    s as user_id,
-   CAST((1000 + s) as CHAR) as affiliate_code,
+   s + 1000 as affiliate_code,
    NOW() as created
 FROM
    GENERATE_SERIES(1,1000) as s(a);
--- docker-images-flask/app/main.py
+++ docker-images-flask/app/main.py
@@ -1,10 +1,40 @@
 import sys
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_marshmallow.fields import fields
+import datetime
 
-db = SQLAlchemy()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{user}:{password}@{host}/{db_name}".format(**{
+    'user': 'postgres',
+    'password': 'postgres',
+    'host': 'postgres-db',
+    'db_name': 'POSTGRESDB'
+})
+app.config['JSON_AS_ASCII'] = False  # JSONの文字化け対策
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+ma = Marshmallow(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    affiliate_code = db.Column(db.Integer)
+    created = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        model = User
+        fields = ('user_id', 'affiliate_code', 'created')
 
 
 @app.route("/")
@@ -14,6 +44,35 @@ def hello():
         version
     )
     return message
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': '日用品を買ってくる',
+        'description': 'ミルク、チーズ、ピザ、フルーツ',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Python の勉強',
+        'description': 'Python で Restful API を作る',
+        'done': False
+    }
+]
+@app.route("/tasks")
+def tasks_handler():
+    return jsonify(tasks=tasks)
+
+
+@app.route("/db")
+def db_handler():
+    db_user_data = User.query.all()
+    user_schema = UserSchema(many=True)
+    return make_response(jsonify({
+        'code': 200,
+        'users': user_schema.dump(db_user_data)
+    }))
 
 
 if __name__ == "__main__":
